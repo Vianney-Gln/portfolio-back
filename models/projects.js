ca0250a5bed5acba4d5c190a7e24a639ab005c57
Vianney-Gln <vgeloen@gmail.com>
2022-06-26 14:46:39 +0200

    route and model create project ok

--- models/projects.js
+++ models/projects.js
@@ -2,8 +2,21 @@
 const connection = require("../config");
 const db = connection.promise();
 
+/**
+ * Function getting all projects
+ * @returns {promise}
+ */
 const getProjects = () => {
   return db.query("SELECT * FROM projects").then((result) => result[0]);
 };
 
-module.exports = { getProjects };
+const createProject = ({ name, url, urlImage, description, date }) => {
+  return db
+    .query(
+      "INSERT INTO projects (name, url, urlImage, description, date) VALUES (?,?,?,?,?)",
+      [name, url, urlImage, description, date]
+    )
+    .then((result) => result[0]);
+};
+
+module.exports = { getProjects, createProject };
--- routes/projects.js
+++ routes/projects.js
@@ -1,8 +1,9 @@
 const projectRouter = require("express").Router();
 
 // Models
-const { getProjects } = require("../models/projects");
+const { getProjects, createProject } = require("../models/projects");
 
+// Routes getting all projects
 projectRouter.get("/projects", (req, res) => {
   getProjects()
     .then((result) => {
@@ -11,6 +12,19 @@ projectRouter.get("/projects", (req, res) => {
     .catch((err) => {
       console.log(err);
       res.status(404).send("error, projects not founds");
+    });
+});
+
+// Route creating a new project
+projectRouter.post("/projects", (req, res) => {
+  const { name, url, urlImage, description, date } = req.body;
+  createProject({ name, url, urlImage, description, date })
+    .then((result) => {
+      res.status(201).send(`project ${result.insertId} created!`);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(403).send("error create project");
     });
 });
 
