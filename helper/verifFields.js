abbb62564eb46fda153f6d957af8557f24792960
Vianney-Gln <vgeloen@gmail.com>
2022-06-27 11:31:26 +0200

    created JOI validate fontion for project creation

--- helper/verifFields.js
+++ helper/verifFields.js
@@ -7,4 +7,14 @@ const validateIntroFields = (data) => {
   }).validate(data, { abortEarly: false, allowUnknown: true }).error;
 };
 
-module.exports = { validateIntroFields };
+const validateProjectFields = (data) => {
+  return Joi.object({
+    name: Joi.string().presence("required"),
+    url: Joi.string().max(255).presence("required"),
+    urlImage: Joi.string().max(255).presence("required"),
+    date: Joi.string().max(255).presence("required"),
+    description: Joi.string().presence("required"),
+  }).validate(data, { abortEarly: false, allowUnknown: true }).error;
+};
+
+module.exports = { validateIntroFields, validateProjectFields };
